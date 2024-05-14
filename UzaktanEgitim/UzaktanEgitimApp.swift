813b1918f5c4b97fe6da6781c1d260732b088568
Armagan Ercan <armagan.erc@gmail.com>
2024-05-14 09:06:13 -0400

    Initial Commit

---
+++ UzaktanEgitim/UzaktanEgitimApp.swift
@@ -0,0 +1,17 @@
+//
+//  UzaktanEgitimApp.swift
+//  UzaktanEgitim
+//
+//  Created by Armagan Ercan on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct UzaktanEgitimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
